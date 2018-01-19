e5d6c0efbd0edfd50a637d7ae425fe7a7c9fab25
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-19 11:40:52 +0300

    creates a test to save user accounts

--- user_test.py
+++ user_test.py
@@ -22,10 +22,18 @@ class TestUser(unittest.TestCase):
         """
         test_init test case to test if the object is properly initialized
         """
+
         self.assertEqual(self.new_user.first_name,"betty")
         self.assertEqual(self.new_user.last_name,"njonjo")
         self.assertEqual(self.new_user.phone_number,"0710283947")
         self.assertEqual(self.new_user.email,"shishnjonjo@gmail.com")
 
+    def test_save_user(self):
+        """
+        test_save_user test case to test if the user object is saved into the user list
+        """
+        self.new_user.save_user() #save new user account
+        self.assertEqual(len(User.user_list),1)
+        
 if __name__ == '__main__':
     unittest.main()
