77a3f3825d0ac8dcc3c4b81f471cbe253537f840
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-19 10:38:40 +0300

    creates instance variables of class user

--- user.py
+++ user.py
@@ -5,4 +5,16 @@ class User:
     """
 def __init__(self,first_name,last_name,phone_number,email):
     """
-    __init__method helps us define properties
+    __init__method helps us define properties for our objects.
+
+    Args:
+    first_name:New user first name.
+    last_name:New user last name.
+    phone_number:New user phone number.
+    email
+    email:New user email address.
+    """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.phone_number = phone_number
+    self.email = email
