f03ecda4cc04879efc5a2cf5d9837c33bf34c7dc
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-19 11:14:14 +0300

    creates user test file

--- user.py
+++ user.py
@@ -3,18 +3,19 @@ class User:
     """
     Class that generates new instances of users
     """
-def __init__(self,first_name,last_name,phone_number,email):
-    """
-    __init__method helps us define properties for our objects.
+    user_list = [] # Empty user list
+    def __init__(self,first_name,last_name,phone_number,email):
+        """
+        __init__method helps us define properties for our objects.
 
-    Args:
-    first_name:New user first name.
-    last_name:New user last name.
-    phone_number:New user phone number.
-    email
-    email:New user email address.
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.phone_number = phone_number
-    self.email = email
+        Args:
+        first_name:New user first name.
+        last_name:New user last name.
+        phone_number:New user phone number.
+        email
+        email:New user email address.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
