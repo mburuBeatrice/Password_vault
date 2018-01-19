db0ad52267d6a37b8a26cae8956adc39d4e1cf03
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-19 08:05:31 +0300

    adds a class for credentials and one for the user

---
+++ user.py
@@ -0,0 +1,8 @@
+class User:
+
+    """
+    Class that generates new instances of users
+    """
+def __init__(self,first_name,last_name,phone_number,email):
+    """
+    __init__method helps us define properties
