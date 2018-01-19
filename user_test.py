61cb3c4c8a7d94d5687535996ed9e268d4b84477
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-19 11:32:53 +0300

    creates a test to ensure objects are instantiated correctly

---
+++ user_test.py
@@ -0,0 +1,31 @@
+import unittest # imports unittest module
+from user import User #imports the user Class
+
+class TestUser(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+    unittest.TestCase: TestCase that helps in creating tset cases.
+
+    """
+    def setUp(self):
+
+        """
+        Set up method to run before each test case.
+        """
+        self.new_user = User("betty","njonjo","0710283947","shishnjonjo@gmail.com") # create user object
+
+    def test_init(self):
+
+        """
+        test_init test case to test if the object is properly initialized
+        """
+        self.assertEqual(self.new_user.first_name,"betty")
+        self.assertEqual(self.new_user.last_name,"njonjo")
+        self.assertEqual(self.new_user.phone_number,"0710283947")
+        self.assertEqual(self.new_user.email,"shishnjonjo@gmail.com")
+
+if __name__ == '__main__':
+    unittest.main()
