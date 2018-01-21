df761e24d5b2024c151b5ac130cb4f66a72cc0ec
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-21 12:01:03 +0300

    add test and method to delete credentials

--- credentials.py
+++ credentials.py
@@ -4,11 +4,7 @@ class Credentials:
     """
     Class that generates new instances of credentials
     """
-    def tearDown(self):
-        '''
-        tearDown method that does clean up after each test case has run.
-        '''
-    credentials_list= [] # Empty credentials list
+
     def __init__(self,account_name,password):
         """
         __init__method helps us define properties for our objects.
@@ -19,9 +15,45 @@ class Credentials:
         """
         self.account_name = account_name
         self.password = password
+
+    credentials_list= [] # Empty credentials list
     def save_credentials(self):
 
         """
         save_user method saves user objects into user_list
         """
         Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """
+        delete_credentials method deletes a saved credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)
+    # @classmethod
+    # def find_by_account_name(cls,account_name):
+    #     '''
+    #     Method that takes in an account_name and returns a password that matches that number.
+    #
+    #     Args:
+    #         account_name: Account name to search for
+    #     Returns :
+    #         credentials that matches the account name.
+    #     '''
+    #
+    #     for credentials in cls.credentials_list:
+    #         if credentials.account_name == account_name:
+    #             return credentials
+    # @classmethod
+    # def credentials_exist(cls,account_name):
+    #     """
+    #     method that checks if a credential exists from a credentials list.
+    #
+    #     Args:
+    #        account_name: account name to search if it exists
+    #     Return:
+    #        Boolean: True or false depending if the credentials exists
+    #     """
+    #     for credentials in cls.credentials_list:
+    #         if credentials.account_name == account_name:
+    #             return True
+    #     return false
--- credentials_test.py
+++ credentials_test.py
@@ -1,7 +1,7 @@
 import unittest # imports unittest module
 from credentials import Credentials #imports the credentials Class
 
-class TestUser(unittest.TestCase):
+class TestCredentials(unittest.TestCase):
 
     """
     Test class that defines test cases for the credentials class behaviours.
@@ -25,8 +25,11 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(self.new_credentials.account_name,"instagram")
         self.assertEqual(self.new_credentials.password,"boo6254")
-
-    Credentials.credentials_list = []
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list= []
 
     def test_save_credentials(self):
         """
@@ -46,5 +49,35 @@ class TestUser(unittest.TestCase):
         test_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),2)
 
+    def test_delete_credentials(self):
+        """
+        test_delete_credentials to test if we can remove a credentials from our credentials list
+        """
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("account_name","password")#new credentials
+        test_credentials.save_credentials()
+
+        self.new_credentials.delete_credentials()#Deleting a credentials object
+        self.assertEqual(len(Credentials.credentials_list),1)
+    # def test_find_credentials_by_account_name(self):
+    #     '''
+    #     test to check if we can find a credential by account name and display information
+    #     '''
+    #
+    #     self.new_credentials.save_credentials()
+    #     test_credentials = Credentials("x_account","88888") # new credentials
+    #     test_credentials.save_credentials()
+    #
+    #     found_credentials = Credentials.find_by_account_name("x_account")
+    # def test_credentials_exist(self):
+    #     """
+    #     test to check whether we can return a Boolean if we cannot find the credentials
+    #     """
+    #     self.new_credentials.save_credentials()
+    #     test_credentials = Credentials("x_account","88888") #new credentials
+    #     test_credentials.save_credentials()
+    #
+    #     credentials_exist = Credentials.credentials_exist("x_account")
+    #     self.assertTrue(credentials_exist)
 if __name__ == '__main__':
     unittest.main()
